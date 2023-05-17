b3929e183595f36be68095d007dcfe610f5871cc
robot-beep <52836102+robot-beep@users.noreply.github.com>
2023-05-17 15:04:16 -0400

    Update rutina.py

--- rutina.py
+++ rutina.py
@@ -2,23 +2,22 @@ import cyberpi
 from cyberpi import mbot2 as m 
 import time 
 
-
-velocidad_giro = 150
-velocidad_giro_rapido = 160
-velocidad_avanzar = 50
-velocidad_retroceso = 50
+velocidad_giro = 90
+velocidad_avanzar = 20
+velocidad_retroceso = 20
 
 #lista de estádos del sensor de color.
 izquierda = [3,7,11]
 derecha = [14,13,12]
 medio = [0,1,2,4,5,6,8,9,10]
 
+#función del seguidor de lineas
 def seguidor_lineas():
     estado_linea = cyberpi.quad_rgb_sensor.get_line_sta(1)
     #si el sensor está viendo solo una linea a la izquierda 
     if estado_linea in izquierda: 
         cyberpi.mbot2.EM_set_speed(-velocidad_giro, "em2")
-        cyberpi.mbot2.EM_set_speed(velocidad_avanzar, "em1")
+        cyberpi.mbot2.EM_set_speed(0, "em1")
     #si el sensor está viendo solo al medio 
     elif estado_linea in medio: 
         cyberpi.mbot2.EM_set_speed(velocidad_avanzar, "em1")
@@ -26,35 +25,86 @@ def seguidor_lineas():
     #si el sensor está viendo a la derecha 
     elif estado_linea in derecha: 
         cyberpi.mbot2.EM_set_speed(velocidad_giro, "em1")
-        cyberpi.mbot2.EM_set_speed(-velocidad_avanzar, "em2")
+        cyberpi.mbot2.EM_set_speed(0, "em2")
     #si el sensor no está viendo nada
     else: 
         cyberpi.mbot2.EM_set_speed((-velocidad_retroceso), "em1")
         cyberpi.mbot2.EM_set_speed((velocidad_retroceso), "em2")
-        
-def leds_movement(): 
-    cyberpi.console.println("han pasado 10 seg")
-    
-def mensaje(): 
-    cyberpi.broadcast("a")
 
-
+#definimos una lista con los brillos de los leds 
+brillos = [100, 100, 100, 100, 100, 100, 100, 100]
 def eyes_colors(): 
-    pass 
+    cyberpi.ultrasonic2.led_show(brillos, 1)
 
-@cyberpi.event.start
+#mover un servo lentamente
+def servo_lento(incremento, target, delay, servo):
+    estado = cyberpi.mbot2.servo_get(servo)
+    if target < estado :
+        incremento = incremento*-1
+    
+    while target != estado: 
+        cyberpi.mbot2.servo_add(incremento, servo)
+        estado = estado + incremento 
+        time.sleep(delay)
+
+#rutina principal 
+@cyberpi.event.is_press("a")
 def start(): 
-    time.sleep(1)
+    time.sleep(2)
+    #prender las luces de los ojos
+    for i in range(3):
+        cyberpi.ultrasonic2.play("happy", 1)
+    #bajamos el brazo lentamente
+    servo_lento(1, 90, 0.01, 1)
+    cyberpi.broadcast("cabeza2")
+    for i in range(2):
+        cyberpi.mbot2.servo_set(120, 2)
+        time.sleep(0.5)
+        cyberpi.mbot2.servo_set(60, 2)
+        time.sleep(0.5)
+    #avanzar hasta ver la linea
+    line = 15
+    while line == 15: 
+        line = cyberpi.quad_rgb_sensor.get_line_sta(1)
+        cyberpi.mbot2.EM_set_speed(velocidad_avanzar, "em1")
+        cyberpi.mbot2.EM_set_speed(-velocidad_avanzar, "em2")
+    cyberpi.broadcast("cabeza")
     while True: 
         seguidor_lineas()
-        cyberpi.broadcast("a")
-    
-
-@cyberpi.event.greater_than(1, 'timer')
-def leds():
-    leds_movement()
-
-@cyberpi.event.receive("a")
-def callback2():
-    cyberpi.console.println("recibido")
+        if cyberpi.quad_rgb_sensor.is_color("g", "any", 1):
+            break
+    cyberpi.mbot2.drive_power(0,0)
     time.sleep(1)
+    cyberpi.mbot2.drive_power(20, -20)
+    time.sleep(0.5)
+    cyberpi.mbot2.drive_power(0, 0)
+    
+#mover la cabeza al recibir la señal 
+@cyberpi.event.receive("cabeza")
+def cabeza():
+    servo_lento(5, 160, 0.05, 3 )
+    time.sleep(1)
+    servo_lento(5, 20, 0.05, 3 )
+    time.sleep(1)
+    servo_lento(5, 90, 0.05, 3)
+    
+@cyberpi.event.receive("cabeza2")
+def cabeza2():
+    cyberpi.mbot2.servo_set(120, 3)
+    time.sleep(0.3)
+    cyberpi.mbot2.servo_set(60, 3)
+    time.sleep(0.3)
+    cyberpi.mbot2.servo_set(90, 3)
+
+#estado inicial 
+@cyberpi.event.is_press('b')
+def estado_inicial():
+    cyberpi.mbot2.servo_set(90,3)
+    cyberpi.mbot2.servo_set(10, 1)
+    cyberpi.mbot2.servo_set(90, 2)
+
+#al presionar el botón arriba 
+@cyberpi.event.is_press('up')
+def arriba(): 
+    while True: 
+        seguidor_lineas()
