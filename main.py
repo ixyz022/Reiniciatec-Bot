08f921b84af0729486f103f4167a177b1fc3c1c9
Ixyz <82106390+ixyz022@users.noreply.github.com>
2023-06-20 14:03:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from cyberpi import mbot2 as m
 import time 
 
 #funcion para seguir lineas 
-def seguidor_lineas():
-    offset = cp.quad_rgb_sensor.get_offset_track(index = 1)  # Obtenemos el offset
-    speed_adjustment = offset  # Ajustamos la velocidad basándonos en el offset
-    # Configuramos la velocidad de los motores
-    motor_right_speed = -50 + ((speed_adjustment/2)*-1)  # Ajustamos la velocidad del motor derecho
-    motor_left_speed = 50 + ((speed_adjustment/2)*-1)  # Ajustamos la velocidad del motor izquierdo
-    # Controlamos los motores
-    m.drive_speed(motor_left_speed, motor_right_speed)
+def seguidor_lineas(comprobacion):
+    if (not comprobacion):
+        m.drive_speed(0, 0)
+    else :
+        offset = cp.quad_rgb_sensor.get_offset_track(index = 1)  # Obtenemos el offset
+        speed_adjustment = offset  # Ajustamos la velocidad basándonos en el offset
+        # Configuramos la velocidad de los motores
+        motor_right_speed = -50 + ((speed_adjustment/2)*-1)  # Ajustamos la velocidad del motor derecho
+        motor_left_speed = 50 + ((speed_adjustment/2)*-1)  # Ajustamos la velocidad del motor izquierdo
+        # Controlamos los motores
+        m.drive_speed(motor_left_speed, motor_right_speed)
                 
                
 #mover un servo
@@ -38,17 +41,20 @@ def etapa1():
     moverServo(50, "em2") # abrir garra
     time.sleep(1) # espera para poner el prop
     moverServo(90, "em2") # cierra la garra
+    time.sleep(0.5) # espera para poner el prop
+    moverServo(70, "em1") # brazo central en el medio
 
 # Sigue la linea hasta la zona de reciclaje dejando la garra a traves de la compuerta
 def etapa2():
     comprobacion = True
-    white = "white"
+    white = "0xffffff"
     while (True):
         #variable1 = cp.quad_rgb_sensor.get_color_sta(1, index = 1)
-        #variable1 = cp.quad_rgb_sensor.get_color(1, index = 1)
-        #comprobacion = False if variable1 != white else comprobacion
-        #cp.console.println(variable1) # Retornar el nivel de gris
-        seguidor_lineas()
+        variable1 = cp.quad_rgb_sensor.get_color(1, index = 1)
+        comprobacion = False if variable1 != white else comprobacion
+        #cp.console.println(variable1) 
+        seguidor_lineas(comprobacion)
+    moverCorto(30, 1) # Moverse hacia atras un segundo
 
 
 def etapa3():
@@ -58,6 +64,14 @@ def etapa3():
     cp.broadcast("levantar")
     moverCorto(-30, 1) # Moverse hacia atras un segundo
     giro360(40, 2.8)
+    
+def etapa4():
+    comprobacion = True
+    white = "0xffffff"
+    while (True):
+        variable1 = cp.quad_rgb_sensor.get_color(1, index = 1)
+        comprobacion = False if variable1 != white else comprobacion
+        seguidor_lineas(comprobacion)
 
 @cp.event.receive("levantar")
 def etapa3dot5():
@@ -76,5 +90,13 @@ def start2():
 def start3():
     etapa3()
   
-
+@cp.event.is_press('left')    
+def all():
+    etapa1()
+    time.sleep(1) # espera
+    etapa2()
+    time.sleep(1) # espera
+    etapa3()
+    time.sleep(1) # espera
+    etapa4()
 ##########################################################
